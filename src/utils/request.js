51d3cb742b347580f5271584795dbdf363231c68
Derrick Chen <derrick.chen@coyote-x.com>
2024-05-17 14:58:19 -0400

    optimize request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,17 +2,27 @@
 
 // 导入axios  npm install axios
 import axios from "axios";
-// 定义一个变量,记录公共的前缀  ,  baseURL
-const baseURL = "http://localhost:8080";
+
+import { ElMessage } from "element-plus";
+
+// 定义一个变量,记录公共的前缀,  baseURL
+const baseURL = "/api";
 const instance = axios.create({ baseURL });
 
 // 添加响应拦截器
 instance.interceptors.response.use(
     (result) => {
-        return result.data;
+        if (result.data.code === 0) {
+            // success
+            return result.data;
+        } else {
+            // error
+            ElMessage.error(result.data.msg ? result.data.msg : "服务异常");
+            return Promise.reject(result.data); // 异步的状态转化成失败的状态
+        }
     },
     (err) => {
-        alert("服务异常");
+        ElMessage.error("服务异常");
         return Promise.reject(err); // 异步的状态转化成失败的状态
     }
 );
