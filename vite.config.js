d931babd38d3f0d27f2004f1c72a61dd607d7d06
Derrick Chen <derrick.chen@coyote-x.com>
2024-05-17 14:57:58 -0400

    Change origin url from port 5173 to 8080

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+
+    server: {
+        proxy: {
+            // Get '/api' request
+            "/api": {
+                // Backend service url
+                target: "http://localhost:8080/api/v1",
+                // Change origin set to true
+                changeOrigin: true,
+                // Replace '/api' to ''
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
+});
