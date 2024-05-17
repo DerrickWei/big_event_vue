99a7de9c56a26539092fb053f0d6feb75320af77
Derrick Chen <derrick.chen@coyote-x.com>
2024-05-17 16:17:23 -0400

    use persist on pinia

--- src/stores/token.js
+++ src/stores/token.js
@@ -9,23 +9,27 @@ import { ref } from "vue";
     defineStore返回值描述：
         返回的是一个函数，将来可以调用该函数，得到第二个参数中返回的内容
 */
-export const useTokenStore = defineStore("token", () => {
-    // 1.定义描述token
-    const token = ref("");
+export const useTokenStore = defineStore(
+    "token",
+    () => {
+        // 1.定义描述token
+        const token = ref("");
 
-    // 2.定义修改token的方法
-    const setToken = (newToken) => {
-        token.value = newToken;
-    };
+        // 2.定义修改token的方法
+        const setToken = (newToken) => {
+            token.value = newToken;
+        };
 
-    // 3.定义移除token的方法
-    const removeToken = () => {
-        token.value = "";
-    };
+        // 3.定义移除token的方法
+        const removeToken = () => {
+            token.value = "";
+        };
 
-    return {
-        token,
-        setToken,
-        removeToken,
-    };
-});
+        return {
+            token,
+            setToken,
+            removeToken,
+        };
+    },
+    { persist: true }
+);
