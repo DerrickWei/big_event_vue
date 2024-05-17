4feadc38ba1e33fbce9474bfcff8d08f467a98b8
Derrick Chen <derrick.chen@coyote-x.com>
2024-05-17 16:09:51 -0400

    use pinia on main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
 import "./assets/main.scss";
 
 import { createApp } from "vue";
+
+// Element plus
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+
 import App from "./App.vue";
+
+// Vue-router
 import router from "@/router";
+
+// Pinia
+import { createPinia } from "pinia";
 
 const app = createApp(App);
 
 app.use(ElementPlus);
 app.use(router);
+
+const pinia = createPinia();
+app.use(pinia);
 
 app.mount("#app");
