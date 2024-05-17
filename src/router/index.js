68e6d5e360c7c5f068f0338ca18a3182bc41071b
Derrick Chen <derrick.chen@coyote-x.com>
2024-05-17 15:14:19 -0400

    set up children routes for layout vue

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,27 @@ import { createRouter, createWebHistory } from "vue-router";
 import LoginVue from "@/views/Login.vue";
 import LayoutVue from "@/views/Layout.vue";
 
+import ArticleCategoryVue from "@/views/article/ArticleCategory.vue";
+import ArticleVue from "@/views/article/ArticleManage.vue";
+import UserInfoVue from "@/views/user/UserInfo.vue";
+import UserAvatarVue from "@/views/user/UserAvatar.vue";
+import UserResetPasswordVue from "@/views/user/UserResetPassword.vue";
+
 //定义路由关系
 const routes = [
     { path: "/login", component: LoginVue },
-    { path: "/", component: LayoutVue },
+    {
+        path: "/",
+        component: LayoutVue,
+        redirect: "/article/manage",
+        children: [
+            { path: "/article/category", component: ArticleCategoryVue },
+            { path: "/article/manage", component: ArticleVue },
+            { path: "/user/info", component: UserInfoVue },
+            { path: "/user/avatar", component: UserAvatarVue },
+            { path: "/user/resetPassword", component: UserResetPasswordVue },
+        ],
+    },
 ];
 
 //创建路由器
